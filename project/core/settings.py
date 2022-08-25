2e9e7ec45d871a5fcf50242154878992de72f557
Sergey Gurkov <biggreen.rm@gmail.com>
2022-08-25 10:23:51 +0300

    [IMP] main: add CLI command for orders reports

--- project/core/settings.py
+++ project/core/settings.py
@@ -138,3 +138,8 @@ CELERY_BEAT_SCHEDULE = {
         "schedule": crontab(minute="*/1")
     }
 }
+
+# Email
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+DEFAULT_FROM_EMAIL = "noreply@email.com"
+ADMINS = [("testuser", "test.user@email.com"), ]
